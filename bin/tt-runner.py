16bd7be80235a6e4a827ff6c6818b831c8e5a50b
Kazuho Fujii <kazuho.fujii@gmail.com>
2016-07-24 00:14:23 +0900

    Run before/after operations once for all run-operations.

--- bin/tt-runner.py
+++ bin/tt-runner.py
@@ -361,13 +361,11 @@ def create_plan(root_node):
 
         ops_under_node.extend(before_all_ops)
 
+        ops_under_node.extend(before_ops)
         for run_node in sorted_nodes(node.runs):
-            ops_under_node.extend(before_ops)
-
             run_ops = visit(run_node, my_depends)
             ops_under_node.extend(run_ops)
-
-            ops_under_node.extend(after_ops)
+        ops_under_node.extend(after_ops)
 
         for test_node in randomize_nodes(node.tests):
             ops_under_node.extend(before_ops)
