6278f1b75416fc81ec3b03351699039e975f1ffe
Kazuho Fujii <kazuho.fujii@gmail.com>
2016-08-12 20:28:04 +0900

    Skip unexecutable files; do not ignore them

---
+++ test/test-unexecutable.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+. ./functions.sh
+
+set -eux
+
+create_succeeding_test ${WORKDIR}/test1.sh
+chmod 644 ${WORKDIR}/test1.sh
+
+OUT=$(tt-runner ${WORKDIR})
+
+[[ ${OUT} == "1..1
+ok 1 test1.sh # SKIP
+# test1.sh is not executable." ]]
