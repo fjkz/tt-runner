7dbe62340c2152b46b9d006cc73a4f8b5db1d669
Kazuho Fujii <kazuho.fujii@gmail.com>
2016-09-24 04:57:51 +0900

    Convert relative paths in PATH env to be absolute

--- test/exec_tests.sh
+++ test/exec_tests.sh
@@ -4,7 +4,7 @@ MYNAME=$(readlink -f "${BASH_SOURCE:-$0}")
 MYDIR=$(cd -P -- $(dirname -- "${MYNAME}"); pwd)
 cd "${MYDIR}"
 
-export PATH="${MYDIR}/../bin:${PATH}"
+export PATH="../bin:${PATH}"
 export WORKDIR="/tmp/tt-runner-tests"
 export CONFDIR="${MYDIR}/conf"
 rm -rf result
