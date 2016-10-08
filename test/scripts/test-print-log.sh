83e4d857829c9944bca13af66fafa54480592351
Kazuho Fujii <kazuho.fujii@gmail.com>
2016-10-09 02:08:31 +0900

    Write more information to log files

--- test/scripts/test-print-log.sh
+++ test/scripts/test-print-log.sh
@@ -11,8 +11,20 @@ set +e
 ERR=$(ttap ${WORKDIR} --print-log -o ${WORKDIR}/result 2>&1 1>/dev/null)
 set -e
 
-[[ $(cat ${WORKDIR}/result/test1.sh.out) == "OK" ]]
-[[ $(cat ${WORKDIR}/result/test2.sh.out) == "NOT OK" ]]
+[[ $(head -n 7 ${WORKDIR}/result/test1.sh.out) == "file: test1.sh
+
+--- output start ---
+OK
+--- output end ---
+
+status code: 0" ]]
+[[ $(head -n 7 ${WORKDIR}/result/test2.sh.out) == "file: test2.sh
+
+--- output start ---
+NOT OK
+--- output end ---
+
+status code: 1" ]]
 [[ $(cat ${WORKDIR}/result/result.txt) == \
 "ok 1 test1.sh
 not ok 2 test2.sh
